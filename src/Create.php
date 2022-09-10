416f8f853652d1f0c9e6ad57dfb8c4a2001a5529
Clark Rasmussen <cjr@clarkrasmussen.com>
2022-09-09 21:40:05 -0400

    Add a wrapper method for creating an image resource with a transparent background

---
+++ src/Create.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace cjrasmussen\Image;
+
+class Create
+{
+	/**
+	 * Create image resources with transparent background
+	 * 
+	 * @param int $width
+	 * @param int $height
+	 * @return false|\GdImage|resource
+	 */
+	public static function transparent($width, $height)
+	{
+		$img = imagecreatetruecolor($width, $height);
+		imagesavealpha($img, true);
+		imagefill($img, 0, 0, imagecolorallocatealpha($img, 0, 0, 0, 127));
+
+		return $img;
+	}
+}
